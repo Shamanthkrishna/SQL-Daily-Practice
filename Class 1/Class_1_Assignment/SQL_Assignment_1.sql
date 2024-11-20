e625882e54aa9b370af3dec37ff7ccd14c05d82d
Shamanth Krishna <shamanthkrishna0@gmail.com>
2024-11-20 21:31:53 +0530

    Solved Day 3 SQL Problems!!

--- Class 1/Class_1_Assignment/SQL_Assignment_1.sql
+++ Class 1/Class_1_Assignment/SQL_Assignment_1.sql
@@ -390,3 +390,32 @@ SELECT 	a.product_id,
 FROM unitssold a
 JOIN prices b ON a.product_id = b.product_id AND purchase_date BETWEEN start_date AND end_date
 GROUP BY a.product_id;
+
+-- Q24.
+
+create table if not exists Activity
+(
+player_id int,
+device_id int,
+event_date date,
+games_played INT DEFAULT 0,
+constraint pk PRIMARY KEY (player_id, event_date)
+);
+
+INSERT into Activity values
+(1,2,'2016-03-01',5),
+(1,2,'2016-05-02',6),
+(2,3,'2017-06-25',1),
+(3,1,'2016-03-02',0),
+(3,4,'2018-07-03',5);
+
+SELECT 	player_id,event_date AS first_login,
+		ROW_NUMBER() OVER(PARTITION BY player_id) AS row_num
+FROM Activity;
+
+SELECT	tmp.player_id,tmp.event_date AS first_login
+FROM (	SELECT *,
+			ROW_NUMBER() OVER(PARTITION BY player_id ) AS row_num
+		FROM Activity) tmp
+WHERE tmp.row_num = 1;
+
