a646f32af64eb2e9f6397ac96b06a76a14a6ee13
Shamanth Krishna <shamanthkrishna0@gmail.com>
2024-12-04 22:07:22 +0530

    Updated the queries

--- SQL_Exercises/E1/Q&As.sql
+++ SQL_Exercises/E1/Q&As.sql
@@ -41,12 +41,52 @@ where price >= 180
 order by price desc, name;
 
 -- 1.10 Select all the data from the products, including all the data for each product's manufacturer.
+select *
+from products p, manufacturers m
+where p.Manufacturer = m.Code;
+
 -- 1.11 Select the product name, price, and manufacturer name of all the products.
+select p.name, p.price, m.name
+from products p, manufacturers m
+where p.Manufacturer = m.Code;
+-- OR
+select p.name, p.price, m.name
+from products p 
+join manufacturers m on p.Manufacturer = m.Code;
+
 -- 1.12 Select the average price of each manufacturer's products, showing only the manufacturer's code.
+select Manufacturer, avg(price) Avg_Price
+from products
+group by Manufacturer;
+
 -- 1.13 Select the average price of each manufacturer's products, showing the manufacturer's name.
+select m.name Name, avg(p.price) Avg_Price
+from products p 
+join manufacturers m on p.Manufacturer = m.Code
+group by m.name;
+
 -- 1.14 Select the names of manufacturer whose products have an average price larger than or equal to $150.
+select m.name Name, avg(p.price) Avg_Price
+from products p 
+join manufacturers m on p.Manufacturer = m.Code 
+group by m.name
+having Avg_Price >=150;
+
 -- 1.15 Select the name and price of the cheapest product.
+select name, price
+from products
+order by price
+limit 1;
+
+select name, price
+from products
+where price = (select min(price) from products);
+
 -- 1.16 Select the name of each manufacturer along with the name and price of its most expensive product.
+select m.name, p.name, p.price
+from products p
+join manufacturers m on p.Manufacturer = m.Code
+where price = (select max(price) from products WHERE Manufacturer = m.Code);
 -- 1.17 Add a new product: Loudspeakers, $70, manufacturer 2.
 -- 1.18 Update the name of product 8 to "Laser Printer".
 -- 1.19 Apply a 10% discount to all products.
